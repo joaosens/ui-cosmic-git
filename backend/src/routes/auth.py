0b3064f044f4501658d3317d637646edbcaa738f
joaosens <jeduardosensss@gmail.com>
2026-06-04 13:09:48 -0300

    feature: setting auth system - signup and login

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -1,8 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends 
 from src.core.security import create_access_token
+from sqlalchemy.orm import Session
+from src.database.connection import get_db
+from src.database.init_db import init_db
 
+init_db()
 router = APIRouter()
 
+
+@router.post("/auth/signup")
+async def signup(username, password, email, db: Session):
+    db = Depends(get_db)
+    db.add(username, password, email)
+    db.commit()
 @router.post("/auth/login")
 async def login(user_id: str):
     token = create_access_token({"sub": user_id})
