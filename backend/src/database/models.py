14db830ae966345cf304913ff0edca5636f48c5b
joaosens <jeduardosensss@gmail.com>
2026-06-07 01:53:02 -0300

    feat: User relational table ORM

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from datetime import utc
 from src.database.connection import Base
 
 # Class inherit 'declarative_base' is transformed to a table in Database SQL
@@ -11,10 +12,16 @@ class Message(Base):
     content = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now()) # Allows timezone compared UTC, and 'func' save exactly moment inside own SQL not in Python 
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True)
+    created_at = Column(DateTime(timezone=utc), server_default=func.now())
+
 # Token's persistence 
 class UserConfig(Base):
     __tablename__ = "user_configs"
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(String, unique=True, nullable=False, index=True)
+    user_id = Column(String, ForeignKey("users.id"), unique=True, nullable=False, index=True)
     git_token = Column(String, nullable=False)
