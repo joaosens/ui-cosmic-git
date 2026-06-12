edb559dcdee6542153723444b45dd8540455941e
joaosens <jeduardosensss@gmail.com>
2026-06-12 01:07:00 -0300

    security in password with hash, and bcrypt

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from datetime import utc
 from src.database.connection import Base
@@ -16,7 +17,13 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(String, primary_key=True)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=utc), server_default=func.now())
+
+    config = relationship("UserConfig", back_populates="user", uselist=False) 
+    # 'back_populates' connects the variable's relationship of table relationated 
+    # 'uselist' is the information if table is 'Many to many'
 
 # Token's persistence 
 class UserConfig(Base):
